0e1944f93eb6516892f00da3df6f9e89bab9d1b7
MChen0607 <chenmin7689@163.com>
2021-04-11 14:55:48 +0800

    敏感词过滤

---
+++ src/test/java/com/newcoder/community/SensitiveTests.java
@@ -0,0 +1,24 @@
+package com.newcoder.community;
+
+import com.newcoder.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommuntityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+
+    @Test
+    public void testSensitiveFilter(){
+        String text="这里可以※赌※博，可以嫖※娼，可以吸&毒，可以哈哈哈哈哈哈";
+        text= sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+
+
+}
