52470434517210d540fc5b8e909bdcb858735fdc
MChen0607 <chenmin7689@163.com>
2021-04-13 15:37:23 +0800

    添加用户评论功能

--- src/main/java/com/newcoder/community/dao/CommentMapper.java
+++ src/main/java/com/newcoder/community/dao/CommentMapper.java
@@ -11,4 +11,8 @@ public interface CommentMapper {
     List<Comment> selectCommentsByEntity(int entityType,int entityId,int offset,int limit);
 
     int selectCountEntity(int entityType,int entityId);
+
+    int insertComment(Comment comment);
+
+
 }
--- src/main/java/com/newcoder/community/entity/Comment.java
+++ src/main/java/com/newcoder/community/entity/Comment.java
@@ -13,6 +13,7 @@ public class Comment {
     private int entityId;
     private int targetId;
     private String content;
+    private int status;
     private Date createTime;
 
 }
